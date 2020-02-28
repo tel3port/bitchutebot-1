f37b2cffce6f4ad640e58850f42bd22c3e394c89
tel3port <carn3lian@gmail.com>
2020-02-28 23:09:32 +0300

    still messing around with the commentr() method 13

--- MainScript.py
+++ MainScript.py
@@ -205,11 +205,18 @@ class BitchuteBot:
         try:
             gls.sleep_time()
             self.driver.execute_script("window.scrollBy(0,1000)", "")
-            self.driver.switch_to.frame(self.driver.find_element_by_xpath(disqus_iframe_xpath))
-            placeholder_element = WebDriverWait(self.driver, 25).until(EC.frame_to_be_available_and_switch_to_it((By.CLASS_NAME, 'placeholder')))
-            placeholder_element.click()
-            textarea_element = WebDriverWait(self.driver, 25).until(EC.visibility_of_element_located((By.CLASS_NAME, 'textarea')))
-            textarea_element.send_keys(single_comment)
+            # self.driver.switch_to.frame(self.driver.find_element_by_xpath(disqus_iframe_xpath))
+            # placeholder_element = WebDriverWait(self.driver, 25).until(EC.frame_to_be_available_and_switch_to_it((By.CLASS_NAME, 'placeholder')))
+            # placeholder_element.click()
+            # textarea_element = WebDriverWait(self.driver, 25).until(EC.visibility_of_element_located((By.CLASS_NAME, 'textarea')))
+            # textarea_element.send_keys(single_comment)
+            WebDriverWait(self.driver, 20).until(
+                EC.frame_to_be_available_and_switch_to_it((By.XPATH, "//iframe[@class='disq2disqusCommentsHolder']")))
+            WebDriverWait(self.driver, 20).until(EC.frame_to_be_available_and_switch_to_it(
+                (By.XPATH, "//iframe[starts-with(@id,'dsq-app') and @title='Disqus']")))
+            WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable(
+                (By.XPATH, "//div[@class='textarea'][contains(@aria-label,'Join the discussion')]"))).send_keys(
+                single_comment)
             btn_element = self.driver.find_element_by_xpath(btn_xpath)
             gls.sleep_time()
             btn_element.click()
