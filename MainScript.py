810da0ecafaa0681f7b25e4649a7191cf728fcea
tel3port <carn3lian@gmail.com>
2020-02-28 19:25:43 +0300

    still messing around with the commentr() method 1

--- MainScript.py
+++ MainScript.py
@@ -217,7 +217,7 @@ class BitchuteBot:
             gls.sleep_time()
             textarea_element.send_keys(single_comment)
             gls.sleep_time()
-            btn_element = WebDriverWait(self.driver, 15).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, btn_xpath)))
+            btn_element = WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located((By.XPATH, btn_xpath)))
             gls.sleep_time()
             btn_element.click()
 
